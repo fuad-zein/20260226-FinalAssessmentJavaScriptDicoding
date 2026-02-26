70f0d8e7cbc5493a30a50118855be9abff1cb98a
fuadze <fzein.1695@gmail.com>
2026-02-26 13:02:03 +0700

    task 2: code-style

--- 02-code-style/index.js
+++ 02-code-style/index.js
@@ -1,25 +1,23 @@
-const books = {}
+const books = {};
 
 function getBooks() {
-    return books;
+  return books;
 }
 
 function getBookById(id) {
-  let book = books[id]
-
-    if (!book) {
-      return null;
-    }
-
-    return book.id
+  let book = books[id];
+  if (!book) {
+    return null;
+  }
+  return book.id;
 }
 
 function saveBook(book) {
-  books[book.id] = book
+  books[book.id] = book;
 }
 
-saveBook({id: 'book-1', name: 'Book 1'});
-let myBooks = getBooks()
+saveBook({ id: 'book-1', name: 'Book 1' });
+const myBooks = getBooks();
 const myBook = getBookById('book-1');
 
 console.log(myBooks);
